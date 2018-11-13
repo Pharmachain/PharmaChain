6b5dbc5e9fd5f37f30d4b84ef8a0463749caa5b1
mdulin2 <mdulin2@zagmail.gonzaga.edu>
2018-11-13 16:11:38 -0500

    Fix quorum link3

--- test_runner.sh
+++ test_runner.sh
@@ -4,7 +4,7 @@ sudo git clone https://github.com/Pharmachain/quorum-maker
 echo "Clone into quroum-maker"
 cd quorum-maker
 echo "Move into quroum-maker"
-ls
+ls -la
 cd TestNetwork 
 sudo docker-compose up -d 
 exit 0
