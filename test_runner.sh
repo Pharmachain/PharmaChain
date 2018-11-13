f64127a9d1b43ffae7959126c3c386226135ff07
mdulin2 <mdulin2@zagmail.gonzaga.edu>
2018-11-13 16:21:27 -0500

    Moving back into Pharmachain

--- test_runner.sh
+++ test_runner.sh
@@ -4,12 +4,11 @@ sudo git clone https://github.com/Pharmachain/quorum-maker
 echo "Clone into quroum-maker"
 cd quorum-maker
 echo "Move into quroum-maker"
-ls -la
 cd TestNetwork 
 sudo docker-compose up -d
 # Open the nodes 
 
 cd ../..
 cd PharmaChain/
- 
+ls -la 
 exit 0
