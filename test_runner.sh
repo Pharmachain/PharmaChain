7bb836309c34e18d90e425c12b6258dea0967b1a
mdulin2 <mdulin2@zagmail.gonzaga.edu>
2018-11-13 18:46:27 -0500

    Open nodes

--- test_runner.sh
+++ test_runner.sh
@@ -4,11 +4,17 @@ sudo git clone https://github.com/Pharmachain/quorum-maker
 echo "Clone into quroum-maker"
 cd quorum-maker
 echo "Move into quroum-maker"
-cd TestNetwork 
+cd TestNetwork
+
+# Turn on the Quorum Network  
 sudo docker-compose up -d
+
+# Open the nodes up to be used
+./modular.sh
 # Open the nodes 
 
 cd ../..
 cd PharmaChain/
-ls -la 
-exit 0
+ls -la
+truffle test 
+exit $?
