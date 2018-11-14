c47208ba36a9f2c0bb6ff0ae331534c96021cc18
mdulin2 <mdulin2@zagmail.gonzaga.edu>
2018-11-13 19:07:21 -0500

    Adding geth4

--- test_runner.sh
+++ test_runner.sh
@@ -1,5 +1,4 @@
-sudo apt-get install software-properties-common
-sudo apt-get-repository -y ppa:ethereum/ethereum
+apt-get-repository -y ppa:ethereum/ethereum
 sudo apt-get update
 sudo apt-get install ethereum
 
