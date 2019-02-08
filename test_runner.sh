2a0ebf910866f1cbada48af2246a610f023cf059
root <jkilfoyle@zagmail.gonzaga.edu>
2019-02-07 16:52:17 -0800

    travis test

--- test_runner.sh
+++ test_runner.sh
@@ -4,12 +4,12 @@ cd ~/build/quorum-maker/TestNetwork
 
 # Turn on the Quorum Network  
 docker-compose up -d
-sleep 5m
+sleep 1m
 
 # Open the nodes up to be used
 ./modular.sh
 # Open the nodes 
 
 cd ~/build/Pharmachain/PharmaChain
-truffle version
-truffle test 
+sudo truffle version
+sudo truffle test 
